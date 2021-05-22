dd05c98658d48f7dc02cfee6df4e500665125ce4
coderhglee <goodna17@gmail.com>
2021-05-22 12:21:13 +0900

    feat: 개발 환경을 위한 컨테이너 정의
    - docker compose 추가
    - db 환경 변수 추가

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,21 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RedisClient } from 'redis';
-import { REDIS_CLIENT } from 'src/publish/infra/publish-event.constants';
 
 @Module({
   imports: [
     ConfigModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'feed-db',
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-      synchronize: true,
-      keepConnectionAlive: true,
-      retryAttempts: 2,
-      retryDelay: 1000,
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: config.get('DATABASE_HOST') || 'localhost',
+          database: config.get('MYSQL_DATABASE') || 'feed',
+          username: config.get('MYSQL_USER_NAME') || 'root',
+          password: config.get('MYSQL_ROOT_PASSWORD') || 'root',
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true,
+          keepConnectionAlive: true,
+          retryAttempts: 2,
+          retryDelay: 1000,
+          logging: true,
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
 })
